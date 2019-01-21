8b5de3d54133e3e1ff5c01065d413e742705be45
Andriy Knysh <aknysh@users.noreply.github.com>
2019-01-20 19:24:42 -0800

    Update variables.tf
    Co-Authored-By: osterman <erik@cloudposse.com>

--- variables.tf
+++ variables.tf
@@ -319,7 +319,7 @@ variable "region" {
 
 variable "parent_zone_id" {
   type        = "string"
-  description = "The zone ID where the DNS record for the `short_name` will be written`."
+  description = "The zone ID where the DNS record for the `short_name` will be written"
   default     = ""
 }
 
