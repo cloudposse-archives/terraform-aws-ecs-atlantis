36d607cadd5d7a4928cb74dc12a1a8583de3658f
Igor Rodionov <goruha@gmail.com>
2019-08-20 21:28:31 +0600

    Pin the version

--- main.tf
+++ main.tf
@@ -76,7 +76,7 @@ module "webhooks" {
 }
 
 module "web_app" {
-  source     = "git::https://github.com/cloudposse/terraform-aws-ecs-web-app.git?ref=fix-security-groups"
+  source     = "git::https://github.com/cloudposse/terraform-aws-ecs-web-app.git?ref=tags/0.23.0"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
   name       = "${var.name}"
