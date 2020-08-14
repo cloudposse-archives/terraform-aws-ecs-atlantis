d12b45db6323925971da6a7a956f1b684cdde80b
Matt Gowie <matt@masterpoint.io>
2020-08-14 09:51:57 -0600

    [AUTOMATED] Update terraform-null-label versions to support Terraform 0.13

--- examples/without_authentication/main.tf
+++ examples/without_authentication/main.tf
@@ -57,7 +57,7 @@ module "alb" {
 }
 
 module "ecs_cluster_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.17.0"
   name       = var.name
   namespace  = var.namespace
   stage      = var.stage
