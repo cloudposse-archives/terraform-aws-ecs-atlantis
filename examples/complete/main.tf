e78761e66bdb86aef33a07ac2c683eae6f984668
Max Lobur <max_lobur@outlook.com>
2023-04-28 21:46:28 +0300

    fix tests

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -15,8 +15,8 @@ module "subnets" {
   version              = "2.1.0"
   availability_zones   = var.availability_zones
   vpc_id               = module.vpc.vpc_id
-  igw_id               = module.vpc.igw_id
-  ipv4_cidr_block      = module.vpc.vpc_cidr_block
+  igw_id               = [module.vpc.igw_id]
+  ipv4_cidr_block      = [module.vpc.vpc_cidr_block]
   nat_gateway_enabled  = true
   nat_instance_enabled = false
 
